427925e01e5e3b05a47a5238bee898141504292a
Micheal Adeyinka <michealadeyinka@Micheals-MBP.home>
2022-05-08 14:34:13 +0200

    delete value instead of resetting

--- util.js
+++ util.js
@@ -18,18 +18,3 @@ export const performOperation = (currentValue, previousVal, operator) => {
         return previousVal / Number(currentValue);
     }
 }
-
-export const handleSpecialOperations = (operator, currentValue) => {
-    console.log('currentValue', currentValue)
-    if(operator === '!'){
-        console.log('enetered')
-        // return -parseInt(currentValue)
-        return -Math.abs(currentValue)
-    }
-
-    if(operator === '%'){
-        return Number(currentValue) / 100;
-    }
-}
-
-// console.log('guy', performOperation(0.05, 24, '*'))
